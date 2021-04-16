69df159b0fe15a98be7cfd2a3ef46de129ee77c1
tedo74 <teddy2@abv.bg>
2021-04-16 14:53:56 +0300

    carousel changes

--- assigment1/script.js
+++ assigment1/script.js
@@ -2,9 +2,10 @@ const imgageContainer = document.querySelector('#imgs');
 const btns = document.querySelectorAll('.btn');
 const img = document.querySelectorAll('#imgs img');
 const carouselText = document.querySelectorAll('.carousel-text');
-const member = document.querySelector('.member-quotation-name h6');
+const members = document.querySelectorAll('.member-quotation-name h6');
 
 let idx = 0;
+// setMemberName(idx);
 
 let interval = setInterval(run, 5000);
 
@@ -16,7 +17,7 @@ function resetInterval() {
 function run() {
   idx++;
   changeContent();
-  setMemberName(idx);
+  // setMemberName(idx);
 }
 
 function changeContent() {
@@ -25,15 +26,21 @@ function changeContent() {
   } else if (idx < 0) {
     idx = img.length - 1;
   }
+
   let currentImg = img[idx];
   currentImg.classList.add('active');
+
   let text = carouselText[idx];
   text.classList.add('active-text');
+
+  let member = members[idx];
+  member.classList.add('active-member');
 
   for (let i = 0; i < img.length; i++) {
     if (i !== idx) {
       img[i].classList.remove('active');
       carouselText[i].classList.remove('active-text');
+      members[i].classList.remove('active-member');
 
       btns.forEach((button, index) => {
         button.classList.remove('active');
@@ -42,11 +49,15 @@ function changeContent() {
       carouselText.forEach((txt, index) => {
         txt.classList.remove('active-text');
       });
+      members.forEach((member, index) => {
+        member.classList.remove('active-member');
+      });
     }
   }
   btns[idx].classList.add('active');
   carouselText[idx].classList.add('active-text');
-  setMemberName(idx);
+  members[idx].classList.add('active-member');
+  // setMemberName(idx);
 }
 
 function btnRemoveActive() {}
@@ -62,20 +73,34 @@ btns.forEach((button, index) => {
   });
 });
 
-function setMemberName(index) {
-  switch (index) {
-    case 0:
-      member.textContent = 'John Doe';
-      break;
-    case 1:
-      member.textContent = 'Johny Doe';
-      break;
-    case 2:
-      member.textContent = 'Johnathan Doe';
-      break;
+// function setMemberName(index) {
+//   const m = member;
+//   m.style.opacity = 0;
+//   switch (index) {
+//     case 0:
+//       setTimeout(function () {
+//         m.textContent = 'John Doe';
+//         m.style.opacity = 1;
+//       }, 1100);
+//       // member.textContent = 'John Doe';
+//       break;
+//     case 1:
+//       setTimeout(function () {
+//         m.textContent = 'Johny Doe';
+//         m.style.opacity = 1;
+//       }, 1100);
+//       // member.textContent = 'Johny Doe';
+//       break;
+//     case 2:
+//       setTimeout(function () {
+//         m.textContent = 'Johnathan Doe';
+//         m.style.opacity = 1;
+//       }, 1100);
+//       // member.textContent = 'Johnathan Doe';
+//       break;
 
-    default:
-      member.textContent = 'John Doe';
-      break;
-  }
-}
+//     default:
+//       m.textContent = 'Default';
+//       break;
+//   }
+// }
