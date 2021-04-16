3ca523bb3cf8c1ef12823ca6c991a3557a4315d8
tedo74 <teddy2@abv.bg>
2021-04-16 12:18:35 +0300

    carousel work

--- assigment1/script.js
+++ assigment1/script.js
@@ -1,6 +1,8 @@
 const imgageContainer = document.querySelector('#imgs');
 const btns = document.querySelectorAll('.btn');
 const img = document.querySelectorAll('#imgs img');
+const carouselText = document.querySelectorAll('.carousel-text');
+const member = document.querySelector('.member-quotation-name h6');
 
 let idx = 0;
 
@@ -13,11 +15,11 @@ function resetInterval() {
 
 function run() {
   idx++;
-
-  changeImage();
+  changeContent();
+  setMemberName(idx);
 }
 
-function changeImage() {
+function changeContent() {
   if (idx > img.length - 1) {
     idx = 0;
   } else if (idx < 0) {
@@ -25,23 +27,33 @@ function changeImage() {
   }
   let currentImg = img[idx];
   currentImg.classList.add('active');
+  let text = carouselText[idx];
+  text.classList.add('active-text');
 
   for (let i = 0; i < img.length; i++) {
     if (i !== idx) {
       img[i].classList.remove('active');
+      carouselText[i].classList.remove('active-text');
+
       btns.forEach((button, index) => {
         button.classList.remove('active');
+      });
+
+      carouselText.forEach((txt, index) => {
+        txt.classList.remove('active-text');
       });
     }
   }
   btns[idx].classList.add('active');
+  carouselText[idx].classList.add('active-text');
+  setMemberName(idx);
 }
 
 function btnRemoveActive() {}
 btns.forEach((button, index) => {
   button.addEventListener('click', (evt) => {
     idx = index;
-    changeImage();
+    changeContent();
     btns.forEach((b) => {
       b.classList.remove('active');
     });
@@ -49,3 +61,21 @@ btns.forEach((button, index) => {
     resetInterval();
   });
 });
+
+function setMemberName(index) {
+  switch (index) {
+    case 0:
+      member.textContent = 'John Doe';
+      break;
+    case 1:
+      member.textContent = 'Johny Doe';
+      break;
+    case 2:
+      member.textContent = 'Johnathan Doe';
+      break;
+
+    default:
+      member.textContent = 'John Doe';
+      break;
+  }
+}
